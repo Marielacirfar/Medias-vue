050bd4311ba002b3ce9645d3345a8c8a5f810845
Mariela Farina <mariela.l.farina1@gmail.com>
2023-07-04 10:26:28 -0300

    Inicializando proyecto vue

---
+++ main.js
@@ -0,0 +1,36 @@
+const app= Vue.createApp({
+    data(){
+        return{
+            cart:0,
+            product: 'Socks',
+            brand:'Vue Mastery',
+            selectdVariant: 0,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants:[
+                {id: 2234, color:'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                {id: 2235, color:'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+            ]
+        }
+    },
+    methods:{
+        addToCart(){
+            this.cart +=1
+        },
+        updateVariant(index){
+            this.selectdVariant = index
+            
+
+        }
+    },
+    computed:{
+        title(){
+            return this.brand + ' ' + this.product
+        },
+        image(){
+            return this.variants[this.selectdVariant].image
+        },
+        inStock(){
+            return this.variants[this.selectdVariant].quantity
+        }
+    }
+})
